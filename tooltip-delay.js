a4dec782889a356e127bae48e53db8dc1ddba7b9
tetraDB <dbrown256@gmail.com>
2014-05-14 13:57:07 +0100

    Update tooltip-delay.js
    renamed to showDelay to be inline with hideDelay.
    option is now an object with 'delay' and 'delayOnMove' properties for added behavior.
    Default behavior is same as original.
    
    new usage is 
    showDelay:{
    delay:millisecs(default:1000)
    delayOnMove: true(default)/false
    }
    
    {delayOnMove: false} results in no delay as the mouse is moved along the chart.

--- tooltip-delay.js
+++ tooltip-delay.js
@@ -1,27 +1,25 @@
 (function (H) {
-
     H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
-
-
         var point = arguments[1];
         var chart = this.chart;
         var tooltip = this;
         var refreshArguments = arguments;
-        var delayForDisplay = chart.options.tooltip.delayForDisplay;
-
-
-        if (delayForDisplay) {
-            window.setTimeout(function () {
-
-                if (point === chart.hoverPoint || $.inArray(chart.hoverPoint, point) > -1) {
-                    proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
-                }
-
-            }, delayForDisplay || 1000);
+        if (typeof chart.options.tooltip.showDelay === 'object')
+        {
+            var delay = H.pick(chart.options.tooltip.showDelay.delay, 1000);
+            var delayOnMove = H.pick(chart.options.tooltip.showDelay.delayOnMove, true);
+            if (tooltip.isHidden || delayOnMove) {
+                clearTimeout(tooltip.showTimer);
+                tooltip.showTimer = setTimeout(function () {
+                    if (point === chart.hoverPoint || $.inArray(chart.hoverPoint, point) > -1) {
+                        proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+                    }
+                }, delay);
+            } else {
+                proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+            }
         } else {
             proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
         }
-
     });
-
 }(Highcharts));
